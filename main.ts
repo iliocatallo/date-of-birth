8074c8e3e8196938f46d9278b53093bd22ab5ca5
Ilio Catallo <ilio.catallo@gmail.com>
2022-02-25 12:27:11 +0100

    Add non-working example

--- main.ts
+++ main.ts
@@ -2,4 +2,7 @@ import { DateOfBirth } from "./DateOfBirth";
 
 const dob = DateOfBirth.of({ day: 29, month: 1, year: 1986 })
 
+// This causes a compile-time error
+// DateOfBirth.of({ day: 29, month: 2, year: 1986})
+
 console.log(dob)
